40772c83269a4c32fd345bda00a62e4f5e4b3f66
WuJerry <wujerry116@163.com>
2016-08-15 18:33:56 +0800

    add excel style select

--- src/scripts/designetable.js
+++ src/scripts/designetable.js
@@ -3,6 +3,8 @@
     const q = document.querySelectorAll.bind(document);
     const qe = document.querySelector.bind(document);
     const assign = Object.assign;
+    const SelectedTD = 'selected';
+    const SelectedTDSeletor = '.' + SelectedTD;
 
     class Designetable {
 
@@ -49,34 +51,84 @@
 
 
         _bindEvent() {
-            let el = this.element;
-            let mouseMove = (e) => {
-                if (e.target.tagName === 'TD') {
-                    let movedTd = e.target;
-                    let x = movedTd.cellIndex;
-                    let y = movedTd.parentElement.rowIndex;
-                    if (this.startX !== x || this.startY !== y) {
-                        movedTd.classList.add('selected');
+            let self = this,
+                table = self.element,
+                path = [],
+                addSelectedClass = (el)=> {
+                    el.classList.add(SelectedTD);
+                },
+                clearSelected = ()=> {
+                    self.element.querySelectorAll(SelectedTDSeletor).forEach(item => item.classList.remove(SelectedTD));
+                },
+                renderPath = (x, y, el) => {
+                    let prePoint = path[path.length - 1];
+                    let curPoint = [x,y];
+                    path.push(curPoint);
+
+                    addSelectedClass(el);
+
+
+                    let tempEl = el,
+                        count = 0,
+                        flag = true;
+                    if(y - prePoint[1]){
+                        count = x - self.startX;
+                        let flag = count > 0;
+                        count = Math.abs(count);
+                        console.log('x' + count);
+                        while (count) {
+                            tempEl = flag ? tempEl.previousSibling : tempEl.nextSibling;
+                            addSelectedClass(tempEl);
+                            count --;
+                        }
                     }
-                }
-            };
-            el.addEventListener('mousedown', e => {
-                if (e.target.tagName === 'TD') {
-                    let td = e.target;
-                    td.classList.add('selected');
-                    this.startX = td.cellIndex;
-                    this.startY = td.parentElement.rowIndex;
 
-                    el.addEventListener('mousemove', mouseMove);
+                    if(x - prePoint[0]){
+                        tempEl = el;
+                        count = y - self.startY;
+                        flag = count > 0;
+                        count = Math.abs(count);
+                        console.log('y' + count);
+                        while (count) {
+                            tempEl = flag ? tempEl.parentNode.previousSibling.querySelector('td:nth-child(' + (x + 1) + ')'):
+                                tempEl.parentNode.nextSibling.querySelector('td:nth-child(' + (x + 1) + ')');
+                            addSelectedClass(tempEl);
+                            count --;
+                        }
+                    }
+                },
+                mouseMove = e=> {
+                    if (e.target.tagName === 'TD') {
+                        let movedTd = e.target,
+                            lastPath = path[path.length - 1],
+                            x = movedTd.cellIndex,
+                            y = movedTd.parentElement.rowIndex;
+                        if(lastPath[0] !== x || lastPath[1] !== y){
+                            renderPath(x, y, movedTd);
+                        }
+                    }
+                },
+                mousedown = e => {
+                    if (e.target.tagName === 'TD') {
+                        clearSelected();
+                        let td = e.target;
+                        self.startX = td.cellIndex;
+                        self.startY = td.parentElement.rowIndex;
+                        path.length = 0;
+                        path.push([self.startX, self.startY]);
+                        addSelectedClass(td);
 
-                    //noinspection JSCheckFunctionSignatures
-                    document.addEventListener('mouseup', ()=> {
-                        el.removeEventListener('mousemove', mouseMove, false);
-                    }, {once: true})
+                        table.addEventListener('mousemove', mouseMove);
 
-                }
-            });
+                        //noinspection JSCheckFunctionSignatures
+                        document.addEventListener('mouseup', ()=> {
+                            table.removeEventListener('mousemove', mouseMove, false);
+                        }, {once: true})
 
+                    }
+                };
+
+            table.addEventListener('mousedown', mousedown);
 
         }
 
@@ -114,7 +166,7 @@
 
 //test
 
-    let dt = new Designetable(null, {rows: 3});
+    let dt = new Designetable(null, {rows: 10});
 
     // qe('body').appendChild(dt.element);
 
